90d568f14a109a36a48dcdc3ac0bc4cbed495410
ii iiiii <isac.super@centrum.cz>
2026-08-21 19:24:00 +0200

    Set up GitHub Pages deployment with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { PAPER } from './scripts/icon.js';
 
 export default defineConfig({
+  base: '/ootd/',
   plugins: [
     react(),
     tailwindcss(),
